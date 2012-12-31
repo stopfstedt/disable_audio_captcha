8b03b99e0e7b809d3ba19c0b2021647317478a96
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2012-12-31 09:17:29 -0800

    numerous bugfixes: - replaced calls to on() function with bind() equivalent in order to support older version of jQuery - added code to unbind previously attached click event handlers - added code to prevent event bubbling in onclick handler function

--- js/disable_audio_captcha.js
+++ js/disable_audio_captcha.js
@@ -6,8 +6,11 @@ Drupal.behaviors.disableAudioCaptcha = function (context) {
      jQuery('a.mollom-audio-captcha').each(function () {
        // add a class selector so the link can be restyled (e.g. text strikethrough)
        jQuery(this).addClass('disabled-mollom-audio-captcha');
+       // unbind any previously attached click event handlers
+       jQuery(this).unbind("click");
        // disable click events
-       jQuery(this).on("click", function (event) {
+       jQuery(this).bind("click", function(event) {
+         event.stopPropagation();
          event.preventDefault();
        });
      });
